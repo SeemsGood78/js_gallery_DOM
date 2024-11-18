9402d6cae59f9817a656b8c88c7d26aa3022f473
Pablo <SeemsGood783@gmail.com>
2024-11-18 16:50:25 +0200

    make build

---
+++ index.300003d9.js
@@ -0,0 +1,2 @@
+var e=document.getElementById("thumbs"),t=document.getElementById("largeImg");e.addEventListener("click",function(e){if("IMG"===e.target.tagName||"A"===e.target.tagName){e.preventDefault();var a="IMG"===e.target.tagName?e.target.parentNode.href:e.target.href;t.src=a}});
+//# sourceMappingURL=index.300003d9.js.map
